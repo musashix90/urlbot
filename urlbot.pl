147d2decc44778992f4d734e9ba62bbfa76d1120
Michael Tanner <musashix90@li37-233.members.linode.com>
2008-11-23 13:27:16 -0500

    Just some cosmetic changes to the code

--- urlbot.pl
+++ urlbot.pl
@@ -24,35 +24,35 @@ my %flood;
 our ($irc) = POE::Component::IRC->spawn();
 
 POE::Session->create(
-    inline_states => {
-        _start     => \&bot_start,
-	connect	   => \&bot_connect,
-        irc_001    => \&on_connect,
-	irc_433	   => \&err_nickinuse,
-	irc_471	   => \&err_chan,
-        irc_473    => \&err_chan,
-        irc_474    => \&err_chan,
-        irc_475    => \&err_chan,
-        irc_public => \&on_public,
-        irc_ctcp_version => \&on_version,
-        irc_ctcp_time => \&on_time,
-	irc_msg => \&on_privmsg,
-	irc_notice => \&on_notice,
-	irc_error => \&bot_reconnect,
-	irc_socketerr => \&bot_reconnect,
-	irc_ctcp_action => \&on_action,
-        irc_page_title => \&irc_page_title,
-        irc_kick   => \&on_kick,
-    },
+     inline_states => {
+          _start              => \&bot_start,
+     	connect	          => \&bot_connect,
+          irc_001             => \&on_connect,
+     	irc_433	          => \&err_nickinuse,
+     	irc_471	          => \&err_chan,
+          irc_473             => \&err_chan,
+          irc_474             => \&err_chan,
+          irc_475             => \&err_chan,
+          irc_public          => \&on_public,
+          irc_ctcp_version    => \&on_version,
+          irc_ctcp_time       => \&on_time,
+          irc_msg             => \&on_privmsg,
+          irc_notice          => \&on_notice,
+          irc_error           => \&bot_reconnect,
+          irc_socketerr       => \&bot_reconnect,
+          irc_ctcp_action     => \&on_action,
+          irc_page_title      => \&irc_page_title,
+          irc_kick            => \&on_kick,
+     },
 );
 
 sub bot_start {
-    my $kernel  = $_[KERNEL];
-    my $heap    = $_[HEAP];
-    my $session = $_[SESSION];
+     my $kernel  = $_[KERNEL];
+     my $heap    = $_[HEAP];
+     my $session = $_[SESSION];
 
-    $irc->yield( register => "all" );
-    $irc->plugin_add('get_page_title' =>
+     $irc->yield( register => "all" );
+     $irc->plugin_add('get_page_title' =>
                        POE::Component::IRC::Plugin::WWW::GetPageTitle->new(
                          response_event   => 'irc_page_title',
                          auto => 0,
@@ -62,10 +62,10 @@ sub bot_start {
                          listen_for_input => [ qw(public) ],
                          eat => 0,
                          trigger   => qr/^/,
-                      ),
+                       ),
                    );
 
-    $kernel->yield("connect");
+     $kernel->yield("connect");
 }
 sub bot_connect {
 	my ( $kernel, $heap ) = @_[ KERNEL, HEAP ];
@@ -80,39 +80,40 @@ sub bot_connect {
 	);
 }
 sub on_connect {
-    $irc->yield( join => CHANNEL );
-    $irc->yield( mode => $botnick => '+B' );
+     $irc->yield( join => CHANNEL );
+     $irc->yield( mode => $botnick => '+B' );
 }
 
 sub daemonize() {
-        close STDIN;
-        close STDOUT;
-        close STDERR;
-        open(STDIN, '>', '/dev/null');
-        open(STDOUT, '>', '/dev/null');
-        open(STDERR,  '>', '/dev/null');
-        if(fork()) { exit(0) }
+     close STDIN;
+     close STDOUT;
+     close STDERR;
+     open(STDIN, '>', '/dev/null');
+     open(STDOUT, '>', '/dev/null');
+     open(STDERR,  '>', '/dev/null');
+     if(fork()) { exit(0) }
 }
+
 daemonize() if !defined($debug);
 $poe_kernel->run();
 exit 0;
 
 sub on_public {
-        my ( $kernel, $who, $where, $msg ) = @_[ KERNEL, ARG0, ARG1, ARG2 ];
-        my $nick = ( split /!/, $who )[0];
-        my $channel = $where->[0];
+     my ( $kernel, $who, $where, $msg ) = @_[ KERNEL, ARG0, ARG1, ARG2 ];
+     my $nick = ( split /!/, $who )[0];
+     my $channel = $where->[0];
 
-        my $ts = scalar localtime;
-        print " [$ts] <$nick:$channel> $msg\n" if defined($debug);
-        my @args = split(/ /,$msg,4) if length($msg) > 0;
+     my $ts = scalar localtime;
+     print " [$ts] <$nick:$channel> $msg\n" if defined($debug);
+     my @args = split(/ /,$msg,4) if length($msg) > 0;
 	if ($msg =~ /$botnick\: ping\?/i) {
 		$irc->yield(privmsg => $channel => "$nick: Pong!");
 	}
 }
 sub on_privmsg {
-        my ( $kernel, $who, $where, $msg ) = @_[ KERNEL, ARG0, ARG1, ARG2 ];
-        my $nick = ( split /!/, $who )[0];
-        my $target = $where->[0];
+     my ( $kernel, $who, $where, $msg ) = @_[ KERNEL, ARG0, ARG1, ARG2 ];
+     my $nick = ( split /!/, $who )[0];
+     my $target = $where->[0];
 
 	my @split = split(/\s/,$msg);
 	if ($split[0] =~ /^(bookmark|bm)$/i) {
@@ -132,10 +133,10 @@ sub on_privmsg {
 	elsif ($split[0] =~ /^help$/i) {
 		if ($split[1] =~ /^bookmark$/i) {
 			if (@split eq 2) {
-        	                $irc->yield(notice => $nick => "Commands:");
-                                $irc->yield(notice => $nick => "  CREATE    Starts the bookmarking system");
-                                $irc->yield(notice => $nick => "  ADD       Adds a bookmark");
-                                $irc->yield(notice => $nick => "  SEARCH    Searches through bookmarks");
+     	          $irc->yield(notice => $nick => "Commands:");
+                    $irc->yield(notice => $nick => "  CREATE    Starts the bookmarking system");
+                    $irc->yield(notice => $nick => "  ADD       Adds a bookmark");
+                    $irc->yield(notice => $nick => "  SEARCH    Searches through bookmarks");
 			}
 			elsif ($split[2] =~ /^create$/) {
 				$irc->yield(notice => $nick => "Creates the database used for bookmarks");
@@ -164,9 +165,9 @@ sub on_privmsg {
 	}
 }
 sub on_notice {
-        my ( $kernel, $who, $where, $msg ) = @_[ KERNEL, ARG0, ARG1, ARG2 ];
-        my $nick = ( split /!/, $who )[0];
-        my $target = $where->[0];
+     my ( $kernel, $who, $where, $msg ) = @_[ KERNEL, ARG0, ARG1, ARG2 ];
+     my $nick = ( split /!/, $who )[0];
+     my $target = $where->[0];
 	if ($msg =~ /nick(.+)type(.+)\/msg NickServ IDENTIFY(.+)password(.+) Otherwise/i && $nick =~ /NickServ/i) {
 		$irc->yield(privmsg => "NickServ" => "identify $nickpass");
 	}
@@ -183,10 +184,10 @@ sub irc_banned {
 }
 sub xrl {
 	my $url = "http://metamark.net/api/rest/simple?long_url=";
-        if ($_[0] =~ /http\:\/\//) { 
+     if ($_[0] =~ /http\:\/\//) { 
 		$url .= $_[0]; 
 	}
-        else { 
+     else { 
 		$url .= "http://".$_[0]; 
 	}
 	my $browser = LWP::UserAgent->new;
@@ -196,21 +197,21 @@ sub xrl {
 	return "$newurl";
 }
 sub on_version {
-        my ($kernel,$who,$where,$msg) = @_[ KERNEL, ARG0, ARG1, ARG2 ];
-        my $nick = (split /!/,$who)[0];
-        my $target = $where->[0];
-        my $ts = scalar localtime;
+     my ($kernel,$who,$where,$msg) = @_[ KERNEL, ARG0, ARG1, ARG2 ];
+     my $nick = (split /!/,$who)[0];
+     my $target = $where->[0];
+     my $ts = scalar localtime;
 
-        print " [$ts] -- CTCP VERSION request from $nick.\n" if defined($debug);
-        $irc->yield(ctcpreply => $nick => 'VERSION URLBot 0.4-poe by MusashiX90');
+     print " [$ts] -- CTCP VERSION request from $nick.\n" if defined($debug);
+     $irc->yield(ctcpreply => $nick => 'VERSION URLBot 0.4-poe by MusashiX90');
 }
 sub on_time {
-        my ($kernel,$who,$where,$msg) = @_[ KERNEL, ARG0, ARG1, ARG2 ];
-        my $nick = (split /!/,$who)[0];
-        my $target = $where->[0];
-        my $ts = scalar localtime;
-        print " [$ts] -- CTCP TIME request from $nick.\n" if defined($debug);
-        $irc->yield(ctcpreply => $nick => 'TIME '.scalar localtime);
+     my ($kernel,$who,$where,$msg) = @_[ KERNEL, ARG0, ARG1, ARG2 ];
+     my $nick = (split /!/,$who)[0];
+     my $target = $where->[0];
+     my $ts = scalar localtime;
+     print " [$ts] -- CTCP TIME request from $nick.\n" if defined($debug);
+     $irc->yield(ctcpreply => $nick => 'TIME '.scalar localtime);
 }
 sub bot_reconnect {
     my $kernel = $_[KERNEL];
@@ -222,11 +223,11 @@ sub on_action {
 	my $channel = $where->[0];
 	my $ts = scalar localtime;
 	if (lc($msg) =~ /http\:\/\// || lc($msg) =~ /www\.*/) {
-                geturl($msg,$channel);
-        }
+          geturl($msg,$channel);
+     }
 }
 sub err_nickinuse {
-        my $kernel = KERNEL;
+     my $kernel = KERNEL;
 	print "Error: Nickname is already in use.\n" if defined($debug);
 	$botnick .= "_" if $botnick !~ /_$/;
 	$irc->yield(nick => $botnick);
@@ -237,24 +238,24 @@ sub err_chan {
 	$irc->delay( [ join => CHANNEL ], 120);
 }
 sub irc_page_title {
-        my $target = $_[ARG0]{channel};
-        my $title = $_[ARG0]{title};
-        my $who = $_[ARG0]{who};
-        $flood{$target} = 0 if !defined($flood{$target});
-        $flood{$who} = 0 if !defined($flood{$who});
-        print "Found: $target -  $title\n" if defined($debug);
-        if (time - $flood{$target} >= 3 && time - $flood{$who} >= 5) {
-                if (time - $flood{$target} >= 5 && $title !~ /DCC SEND/ && $title !~ /(start|stop)keylogger/ && $title !~ /irc\.(.+)\.(.+)/ && $title !~ /^\[[a-z].+\]\s$/) {
-                        $irc->yield(privmsg => $target => $title);
-                }
-                $flood{$target} = time();
-                $flood{$who} = time();
-         }
+     my $target = $_[ARG0]{channel};
+     my $title = $_[ARG0]{title};
+     my $who = $_[ARG0]{who};
+     $flood{$target} = 0 if !defined($flood{$target});
+     $flood{$who} = 0 if !defined($flood{$who});
+     print "Found: $target -  $title\n" if defined($debug);
+     if (time - $flood{$target} >= 3 && time - $flood{$who} >= 5) {
+          if (time - $flood{$target} >= 5 && $title !~ /DCC SEND/ && $title !~ /(start|stop)keylogger/ && $title !~ /irc\.(.+)\.(.+)/ && $title !~ /^\[[a-z].+\]\s$/) {
+               $irc->yield(privmsg => $target => $title);
+          }
+          $flood{$target} = time();
+          $flood{$who} = time();
+      }
 }
 sub on_kick {
-        my $channel = $_[ARG1];
-        my $victim = $_[ARG2];
-        if ($victim eq $botnick) {
-                $irc->delay( [ join => $channel ], 120);
-        }
+     my $channel = $_[ARG1];
+     my $victim = $_[ARG2];
+     if ($victim eq $botnick) {
+          $irc->delay( [ join => $channel ], 120);
+     }
 }
